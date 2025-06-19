93bbc90f2ab34b0b85e03b576c76773a7257be27
Avner Cv <avner.zerdoun@gmail.com>
2025-06-19 19:25:58 +0200

    commit3

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream> 
 
+/*
 int main ()
 {
-    std::cerr << "Hello Word ! " << std::endl;
+    std::cout << "Hello Word ! " << "VTT" << std::endl;
+    std::cout << "PSG CHAMPION" << std::endl;
+    return 0; 
+}
+
+
+ g++ -std=c++17 -Wall -Wextra -Werror main.cpp -o prog = commande pour compiler le code
+ .\prog.exe : permet de lancer le code 
+ 
+
+
+std::cout : affichage stadard
+std::cerr : erreur
+std::clog : journalisation
+std::endl : retour à la ligne + flush
+\n : retour à la ligne
+\t : tabulation 
+*/
+
+
+/*
+Journalisation avec le clog
+
+int main ()
+{
+    std::clog << "Hello Word ! " << std::endl;
+    return 0; 
+}
+*/
+/*
+préfixes pour les bases 
+0b : binaire
+0 : octal
+0x : hexa
+*/
+int main()
+{
+   
+    int file_number = 0b111111111;
+    
+    std::cout << file_number << std::endl;
+    
+    
     return 0; 
 }
